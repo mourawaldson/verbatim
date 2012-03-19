37ccb43e9ae3259bb0a08605f59c775ea491b491
Waldson Moura <mourawaldson@gmail.com>
2012-03-19 01:18:19 -0300

    translate function call fixed

--- content/js/translate.js
+++ content/js/translate.js
@@ -26,5 +26,5 @@ var Verbatim = {
 
 document.addEventListener("DOMContentLoaded", function () {
 	Verbatim.setDefaultSettings();
-	chrome.contextMenus.create({"title": "Translate '%s'", "contexts":["selection"], "onclick": translate});
+	chrome.contextMenus.create({"title": "Translate '%s'", "contexts":["selection"], "onclick": Verbatim.translate});
 });
